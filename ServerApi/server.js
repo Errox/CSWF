5d7906963677b22c1c8ac860e489cfdbc8a66c5d
Ryan <ryangroenewold@hotmail.com>
2021-11-19 17:49:23 +0100

    Added more sites to cors
    A

--- ServerApi/server.js
+++ ServerApi/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: "https://apicswf.herokuapp.com/"
+  origin: ["https://cswf.herokuapp.com/", "https://apicswf.herokuapp.com"]
 };
 
 app.use(cors(corsOptions));
