93d672ce3e76a2267c1ceab3383576df9c81aba8
Ryan <ryangroenewold@hotmail.com>
2021-11-19 17:52:34 +0100

    Wildcard of cors

--- ServerApi/server.js
+++ ServerApi/server.js
@@ -5,7 +5,7 @@ const cors = require("cors");
 const app = express();
 
 var corsOptions = {
-  origin: ["https://cswf.herokuapp.com/", "https://apicswf.herokuapp.com"]
+  origin: "*"
 };
 
 app.use(cors(corsOptions));
